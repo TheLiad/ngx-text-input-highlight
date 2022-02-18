3090379b7fe2768e419871897b8469f51de8089a
Liad Atia <liado90@gmail.com>
2022-02-18 16:40:50 +0200

    init commit

---
+++ projects/ngx-text-input-highlight/src/public-api.ts
@@ -0,0 +1,8 @@
+/*
+ * Public API Surface of ngx-text-input-highlight
+ */
+
+export * from './lib/text-input-highlight.module';
+export * from './lib/text-input-element.directive';
+export * from './lib/text-input-highlight.component';
+export * from './lib/text-input-highlight-container.directive';
